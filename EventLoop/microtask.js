5cbe7d89d7f267205b7c7b4a8be81006b22fb6fb
Alekya Paladugu <92334692+alekyapaladugu@users.noreply.github.com>
2023-07-04 20:33:13 -0700

    Eventloop Commit

---
+++ EventLoop/microtask.js
@@ -0,0 +1,23 @@
+// process.nextTick(() => {
+//     console.log("Inside next tick")
+// })
+// console.log("Outside next tick")
+// Promise.resolve().then(() => {
+//     console.log("Inside Promise")
+// })
+// setTimeout(() => {
+//     console.log('Timer Queue')
+//     process.nextTick(() => {
+//         console.log("Inside next tick1")
+//     })
+// }, 0)
+setTimeout(() => {
+    console.log('Timer Queue2')
+}, 0)
+setImmediate(() => {
+    console.log('Set Immediate Check')
+})
+for(let i=0;i<20000000;i++){}  // To guarantee the order of event loop executions. Timer Queue before Check Queue
+process.nextTick(() => {
+    console.log("Inside next tick2")
+})
